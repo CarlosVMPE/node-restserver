9e4e444ffbb3548fe06e92ffbfc799c86d334acf
Carlos <1310374@utp.edu.pe>
2019-04-07 10:25:31 -0500

    Login normal sección 10

---
+++ server/middlewares/autenticacion.js
@@ -0,0 +1,52 @@
+const jwt = require('jsonwebtoken');
+
+/**
+ * VERIFICAR TOKEN
+ */
+
+let verificaToken = (req, res, next) => {
+    let token = req.get('token'); // leer header
+    // verificar si el token es igual
+    jwt.verify(token, process.env.SEED, (err, decoded) => {
+        if (err) {
+            // No autorizado
+            return res.status(401).json({
+                ok: false,
+                err: {
+                    message: 'Token no válido'
+                }
+            })
+        }
+
+        req.usuario = decoded.usuario;
+        next();
+    });
+
+};
+
+/**
+ * VERIFICA ADMIN ROLE
+ */
+
+let verificaAdminRole = (req, res, next) => {
+
+    let usuario = req.usuario;
+
+    if (usuario.role == "ADMIN_ROLE") {
+        next();
+    } else {
+        return res.json({
+            ok: false,
+            err: {
+                message: 'El usuario no es Administrador'
+            }
+        })
+    }
+
+
+};
+
+module.exports = {
+    verificaToken,
+    verificaAdminRole
+};
--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
-const Usuario = require('../models/usuario');
 const bcrypt = require('bcrypt');
 const _ = require('underscore');
+const Usuario = require('../models/usuario');
+const { verificaToken, verificaAdminRole } = require('../middlewares/autenticacion')
 
-app.get('/usuario', function(req, res) {
+app.get('/usuario', verificaToken, (req, res) => {
 
     // parametros opcionales
     let desde = req.query.desde || 0;
@@ -33,11 +34,9 @@ app.get('/usuario', function(req, res) {
             });
 
         });
-
-
 });
 
-app.post('/usuario', function(req, res) {
+app.post('/usuario', [verificaToken, verificaAdminRole], (req, res) => {
     let body = req.body;
 
     let usuario = new Usuario({
@@ -63,13 +62,13 @@ app.post('/usuario', function(req, res) {
 
 });
 
-app.put('/usuario/:id', function(req, res) {
+app.put('/usuario/:id', [verificaToken, verificaAdminRole], (req, res) => {
 
     let id = req.params.id;
     // agregando al body todos los datos a cambiar menos el password
     let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
 
-    Usuario.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (err, usuarioDB) => {
+    Usuario.findByIdAndUpdate(id, body, { new: true, runValidators: true, context: 'query' }, (err, usuarioDB) => {
 
         if (err) {
             return res.status(400).json({
@@ -86,7 +85,7 @@ app.put('/usuario/:id', function(req, res) {
 
 });
 
-app.delete('/usuario/:id', function(req, res) {
+app.delete('/usuario/:id', [verificaToken, verificaAdminRole], (req, res) => {
     let id = req.params.id;
 
     let cambiaEstado = {
